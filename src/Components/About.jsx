b573e50390ea09bcebc27f891e42e83df6d942b6
Saad Tahmid <63558162+saadtahmid@users.noreply.github.com>
2025-05-12 16:19:06 +0000

    Update personal information and skills in App and About components; change background image in Home component.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a computer science student with a passion for technology and programming. I enjoy learning new skills and applying them to real-world projects.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "C/C++ programming", 
+  "Java programming",
+  "Python programming",
+  "Data structures and algorithms",
+  "Web development",
+  "Database management",
+  "Digital logic design",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about creating user-friendly interfaces that enhance the user experience. I believe that design should be intuitive and accessible to all users.";
 
 const About = () => {
   return (
